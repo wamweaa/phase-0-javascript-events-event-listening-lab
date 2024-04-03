49a98169ab451197f6cadb6d5eaee6f882f5cce7
wamwea <patokinya12@gmail.com>
2024-04-03 14:35:29 +0300

    complete-lab

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const input  = document.getElementById('button')
+    input.addEventListener('click',()=>alert("i was clicked"))
+    
 }
+console.log(addingEventListener())
